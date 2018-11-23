8432bdef90f2504051930136c19ab4792639c74b
cheewaphat <cheewaphat@gmail.com>
2018-11-24 00:41:16 +0700

    login page

--- project101/application/controllers/Auth.php
+++ project101/application/controllers/Auth.php
@@ -9,8 +9,32 @@ class Auth extends CI_Controller {
     }
     
     public function login()
-	{
-		$this->load->view('v_login');
+	{        
+        $this->load->view('v_login');
+    }
+
+    public function chk_login(){
+
+        if( $this->input->post('email') !== FALSE ){
+            $email = $this->input->post('email');
+            $password = $this->input->post('password');
+
+            $rs = $this->db->query("select * from customers where customer_email like ? and customer_password like ? ",
+                array($email,$password)
+            );
+
+            if ( $rs->num_rows() == 0 ){
+                echo "Login Fail";
+            }else{
+                echo "Login Pass";
+            }
+
+        }else{
+
+            redirect('auth/login');
+
+        }
+        
     }
     
     public function logout()
--- project101/application/views/v_login.php
+++ project101/application/views/v_login.php
@@ -6,13 +6,13 @@
   </head>
 
   <body class="text-center">
-    <form class="form-signin">
+   <form class="form-signin" name="frmlogin" action="<?php echo site_url()?>auth/chk_login" method="POST" >
       <img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
       <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-      <label for="inputEmail" class="sr-only">Email address</label>
-      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-      <label for="inputPassword" class="sr-only">Password</label>
-      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+      <label for="email" class="sr-only">Email address</label>
+      <input type="email" id="email" name="email" class="form-control" placeholder="Email address" required autofocus>
+      <label for="password" class="sr-only">Password</label>
+      <input type="password" id="password" name="password" class="form-control" placeholder="Password" required>
       <div class="checkbox mb-3">
         <label>
           <input type="checkbox" value="remember-me"> Remember me
